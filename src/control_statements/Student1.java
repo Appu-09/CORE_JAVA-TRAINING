397ea4feea22b795fee81b7023471763f0764a9b
Aparna <aparna.vemuganti@effigo.in>
2023-01-21 17:42:32 +0530

    added control statements

---
+++ src/control_statements/Student1.java
@@ -0,0 +1,18 @@
+package control_statements;
+
+public class Student1 {
+	public static void main(String[] args) {  
+		String name = "Aparna";  
+		if(name == "Aparna") {  
+		System.out.println("name  is Aparna");  
+		}else if (name  == "Riya") {  
+		System.out.println("name is Riya");  
+		}else if(name  == "Geetha") {  
+		System.out.println("name is Geetha");  
+		}else {  
+		System.out.println(name);  
+		}  
+		}  
+		}  
+
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author EFFIGO
+ *
+ */
+module Control_Statements {
+}
